6ccfc3bf6a8cc7c0c2e1c2622980dbfbe610aaeb
yui-30089247 <30089247@cc.peralta.edu>
2026-02-12 04:03:07 +0000

    Implemented sortScores

--- DynamicGrades/DynamicGrades.cpp
+++ DynamicGrades/DynamicGrades.cpp
@@ -2,6 +2,9 @@
 
 using namespace std;
 
+void getScores(double *arr, int size);
+void sortScores(double *arr, int size);
+
 int main() {
     int numScores;
 
@@ -10,17 +13,25 @@ int main() {
 
     if (numScores < 0) {
         cout << "Enter a positive number." << endl;
-        return;
+    } else {
+        double *scorePtr = nullptr;
+        scorePtr = new double[numScores];
+    
+        getScores(scorePtr, numScores);
+        cout << "The sorted scores are: ";
+        sortScores(scorePtr, numScores);
+        for (int i = 0; i < numScores; i++) {
+            cout << scorePtr[i] << " ";
+        }
     }
 
-    double *scorePtr = nullptr;
-    scorePtr = new double[numScores];
+    return 0;
 }
 
 void getScores(double *arr, int size) {
     for (int i = 0; i < size; i++) {   
         double score; 
-        cout << "Enter a score: ";
+        cout << "Enter a score " << (i+1) << ": ";
         cin >> score;
         if (score < 0) {
             cout << "Enter a positive number." << endl;
@@ -32,5 +43,13 @@ void getScores(double *arr, int size) {
 }
 
 void sortScores(double *arr, int size) {
-
+    for (int i = size - 1; i > 0; i--) {
+        for (int j = 0; j < i; j++) {
+            if (arr[j] > arr[j+1]) {
+                double temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
 }
